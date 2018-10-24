a8db98b84b8a7c8d61a2f5af34e5f3c604c7c359
Stav Iatrop <staviatrop@gmail.com>
2018-10-24 22:28:05 +0300

    Basic indexing function

---
+++ src/indexing.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "basicStructs.h"
+
+indexArray * indexing(reorderedR * ror, int hash1, int hash2) {
+
+	int sizeOfIndex = hash1Range(hash1);
+	indexArray * mainIndexArray = initializeIndexArray(sizeOfIndex);
+
+	int i, whichPsum = 0;
+	for( i = 0; i < sizeOfIndex; i++) {
+
+		if(i == (ror->psum)[whichPsum].h1Res){ 			//index is created when there is a bucket of i's
+
+			relation * rel = getStartOfBucket(ror, whichPsum);
+			int bucketArrSize = hash2Range(hash2);
+			(mainIndexArray->indexes)[i] = initializeIndex(bucketArrSize, rel, i);
+
+			buildIndex(mainIndexArray, ror, i, whichPsum, hash2);
+			whichPsum++;
+			
+		}else { 										//otherwise no index is created for bucket of i's 
+
+			(mainIndexArray->indexes)[i] = initializeIndex(0, NULL, i);
+		}
+	}
+
+	return mainIndexArray;
+
+}
