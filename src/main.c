6364a9fd84e4fe1d2de25deca9fbd54bcc1f0a0e
Mike Xydas <mikevin97@windowslive.com>
2018-11-02 14:59:45 +0200

    Debugging main

--- src/main.c
+++ src/main.c
@@ -38,40 +38,26 @@ int main(int argc, char *argv[])
         relation * sRel =extractRelation(s, args->colS);
 
         //Reordering
-        uint32_t h1 = 0;
+        uint32_t h1 = FIRST_REORDERED;
         reorderedR * RoR = reorderRelation(rRel, &h1);
+        printf("...H1 = %d | PSUM = %d\n", h1, RoR->pSumArr.psumSize);
         reorderedR * RoS = reorderRelation(sRel, &h1);
 
         //Indexing
-        indexArray * indArr = indexing(RoR, h1, 32 - h1);
-        printf("aaaa\n");
-        fflush(stdout);
+        indexArray * indArr = indexing(RoR, h1, 6);
+
+        //for(int i = 0; i < indArr->size; i++) {
+        //        printf("H2 = %u\n", indArr->indexes[i].hash2);
+        //}
         //Searching
         headResult * results = search(indArr, RoS);
 
         //Writing Output
         writeList(results, args->outPath);
 
-        //printRelation(rRel);
-        //printRelation(sRel);
-
         freeTable(r);
         freeTable(s);
         free(args);
-
-        /*headResult * list = initialiseResultHead();
-
-        
-        for(int i = 0; i < 10000000; i++) {
-                rowTuple temp;
-                temp.rowR = (int32_t) i;
-                temp.rowS = (uint32_t) i;
-                pushResult(list, &temp);
-        }
-
-        if(writeList(list, args->outPath) != 0) {
-                return -1;
-        }*/
 
         return 0;
 }
