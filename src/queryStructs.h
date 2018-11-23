4ba208a1ea80ecaf7598e357a4b7a7ed5593a518
Mike Xydas <mikevin97@windowslive.com>
2018-11-23 19:01:34 +0200

    Initialised queryStructs

---
+++ src/queryStructs.h
@@ -0,0 +1,66 @@
+#ifndef QUERY_STRUCTS_H
+#define QUERY_STRUCTS_H
+
+#include <stdint.h>
+#include "basicStructs.h"
+
+#define LESS 1
+#define GREATER 2
+#define EQUAL 3
+
+typedef uint64_t myint_t;
+
+typedef struct Relationsheep {
+        myint_t rows;
+        myint_t cols;
+        myint_t **pointToCols;
+} relationsheep;
+
+typedef struct RelationsheepArray {
+        relationsheep *rels;
+        myint_t numOfRels;
+} relationsheepArray;
+
+typedef struct Inter {
+        myint_t numOfCols;
+        myint_t **rowIds;
+        myint_t *joinedRels;
+} inter;
+
+typedef struct NodeInter {
+        inter *data;
+        nodeInter *next;
+} nodeInter;
+
+typedef struct HeadInter {
+        myint_t numOfIntermediates;
+        nodeInter *start;
+} headInter;
+
+typedef struct ColRel {
+        myint_t rel;
+        myint_t* col;
+} colRel;
+
+typedef struct Filter {
+        colRel participant;
+        char op;
+        myint_t value;
+} filter;
+
+typedef struct Join {
+        colRel participant1;
+        colRel participant2;
+} join;
+
+typedef struct Query {
+        myint_t *rels;
+        myint_t numOfFilters;
+        myint_t numOfJoins;
+        myint_t numOfSums;
+        filter *filters;
+        join *joins;
+        colRel *sums;
+} query;
+
+#endif
